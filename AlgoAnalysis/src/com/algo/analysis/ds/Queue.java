cc8aaa460c59d4802ba4226dd5fb669eb159db63
durga-nayak <hapi.durga@gmail.com>
2015-07-27 16:10:50 +0530

    ds

---
+++ AlgoAnalysis/src/com/algo/analysis/ds/Queue.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.algo.analysis.ds;
+
+/**
+ * @author NAYAKDUR
+ *
+ */
+public interface Queue<E> {
+
+	public void enQueue(E e);
+	public E deQueue();
+}
---
+++ AlgoAnalysis/src/com/algo/analysis/ds/Stack.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.algo.analysis.ds;
+
+/**
+ * @author NAYAKDUR
+ *
+ */
+public interface Stack<E> {
+	public void push(E e);
+	public E pop();
+	public E getSuccessor(int index);
+	public E getPredessor(int index);
+}
